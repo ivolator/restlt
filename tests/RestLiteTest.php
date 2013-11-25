7279f21a60d863bd5ac87e4d2413d2bc786ca59a
ivo <ivo@localhost.localdomain>
2013-11-24 21:10:41 -0500

    IM** move file

---
+++ tests/RestLiteTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class RestLiteTest extends \PHPUnit_Framework_TestCase {
+
+	protected function setUp() {
+		parent::setUp ();
+	}
+
+	public function testSkip(){
+		$this->markTestSkipped();
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		parent::tearDown ();
+	}
+}
