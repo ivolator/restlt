685ba583e9216e80ca70c341367a16117688f79a
Ivo M <mandalski@gmail.com>
2013-12-31 09:30:49 -0500

    IM**  adjust bootstrap to work with travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 ini_set('display_errors', 'off');
-$al = require __DIR__ . '/../../../autoload.php';
+$al = @include __DIR__ . '/../vendor/autoload.php';
+if(!$al){
+	$al = @include __DIR__ . '/../../../autoload.php';
+}
 set_include_path(get_include_path().':' . __DIR__ . '/../lib:' . __DIR__.'/fixtures');
 $al->setUseIncludePath(true);
