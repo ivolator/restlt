2949b2d3fce0b95c3dcff9df74ac27a562120eb6
ivolator <mandalski@gmail.com>
2014-12-27 21:46:13 -0500

    IM** - fix travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 ini_set('display_errors', 'off');
-$al = @include realpath(__DIR__ . '/../../../vendor/autoload.php');
-if(!$al){
-	$al = @include __DIR__ . '/../../../autoload.php';
+$al = @include realpath(__DIR__ . '/../vendor/autoload.php');
+if (! $al) {
+    $al = @include __DIR__ . '/../../../autoload.php';
 }
 
-$al->add ('fixtures\\',__FILE__ . '/');
+$al->add('fixtures\\', __FILE__ . '/');
 
 require_once 'RestLiteTest.php';
-set_include_path(get_include_path().':' . __DIR__ . '/../lib:' . __DIR__.'/fixtures');
+set_include_path(get_include_path() . ':' . __DIR__ . '/../lib:' . __DIR__ . '/fixtures');
 $al->setUseIncludePath(true);
