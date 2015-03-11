92896974471ddf5b79b69ef82a84f33783510bc2
Ivo Mandalski <Ivo.Mandalski@onforce.com>
2015-03-11 09:25:48 -0400

    Revert "IM** test skip, adjust"
    This reverts commit 3bd8788415e925622532f47b09cabc4fe462e740.

--- tests/fixtures/mocks/MockRequestRouter.php
+++ tests/fixtures/mocks/MockRequestRouter.php
@@ -3,39 +3,31 @@ use restlt\routing\RouterInterface;
 class MockRequestRouter implements RouterInterface {
 	protected $request = null;
 	protected $resources = null;
-
+	
 	/* (non-PHPdoc)
 	 * @see restlt\routing.RouterInterface::getRoute()
 	 */
 	public function getRoute() {
 	}
-
+	
 	/* (non-PHPdoc)
 	 * @see restlt\routing.RouterInterface::getRequest()
 	 */
 	public function getRequest() {
 		return $this->request;
 	}
-
+	
 	public function setRequest($request) {
 		$this->request = $request;
 		return $this;
 	}
-
+	
 	public function setResources($res) {
 		$this->resources = $res;
 		return $this;
 	}
-
+	
 	public function getResources() {
 		return $this->resources;
-	}
-
-	public function setCache($cache){
-	    return $this;
-	}
-
-	public function setMetadataBuilder($cache){
-	    return $this;
 	}
 }
